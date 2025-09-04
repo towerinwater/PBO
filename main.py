639c8396aa3c92b6e732f759fb19dfb4ca226927
Tang Piseth <tangpiseth43@gmail.com>
2025-09-04 17:51:00 +0930

    A: getting started

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pbo!")
+
+
+if __name__ == "__main__":
+    main()
--- src/problem_example.py
+++ src/problem_example.py
@@ -1,24 +1,27 @@
 from ioh import get_problem, ProblemClass
-from ioh import logger
+from ioh import logger, iohcpp
 import sys
 import numpy as np
 
 # Please replace this `random search` by your `genetic algorithm`.
-def random_search(func, budget = None) -> tuple[int, int, int]:
+def random_search(func, budget = None) -> tuple[float, np.ndarray, int]:
     # budget of each run: 50n^2
     if budget is None:
         budget = int(func.meta_data.n_variables * func.meta_data.n_variables * 50)
-
+    optimum = 0 
     if func.meta_data.problem_id == 18 and func.meta_data.n_variables == 32:
         optimum = 8
     else:
         optimum = func.optimum.y
     print(optimum)
     # 10 independent runs for each algorithm on each problem.
-    for r in range(10):
-        f_opt = sys.float_info.min
-        x_opt = None
-        for i in range(budget):
+    f_opt = float('-inf')
+    x_opt = np.ndarray([])
+
+    for _ in range(10):
+        f_opt = float('-inf')
+        x_opt = np.ndarray([])
+        for _ in range(budget):
             x = np.random.randint(2, size = func.meta_data.n_variables)
             f = func(x)
             if f > f_opt:
@@ -30,15 +33,16 @@ def random_search(func, budget = None) -> tuple[int, int, int]:
     return f_opt, x_opt, optimum
 
 # Declaration of problems to be tested.
-om = get_problem(fid = 1, dimension=50, instance=1, problem_class = ProblemClass.PBO)
-lo = get_problem(fid = 2, dimension=50, instance=1, problem_class = ProblemClass.PBO)
-labs = get_problem(fid = 18, dimension=50, instance=1, problem_class = ProblemClass.PBO)
+om = get_problem(fid = 1, dimension=50, instance=1, problem_type = ProblemClass.PBO)
+lo = get_problem(fid = 2, dimension=50, instance=1, problem_type = ProblemClass.PBO)
+labs = get_problem(fid = 18, dimension=50, instance=1, problem_type = ProblemClass.PBO)
 
 
+
+root = logger.Path("data")
 # Create default logger compatible with IOHanalyzer
 # `root` indicates where the output files are stored.
-# `folder_name` is the name of the folder containing all output. You should compress this folder and upload it to IOHanalyzer
-l = logger.Analyzer(root="data", 
+l = logger.Analyzer(root=root, 
     folder_name="run", 
     algorithm_name="random_search", 
     algorithm_info="test of IOHexperimenter in python")
